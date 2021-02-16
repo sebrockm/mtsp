a679cd71300f088d939ce7788a2a96b9aa3a56d3
Sebastian Brockmeyer <937096+sebrockm@users.noreply.github.com>
2021-02-17 00:47:30 +0100

    some improvements

--- src/mtsp.py
+++ src/mtsp.py
@@ -14,10 +14,12 @@ args = parser.parse_args()
 N = args.nodes
 A = args.agents
 
+nodes = np.arange(N)
+agents = np.arange(A)
+
 np.random.seed(42)
 
 G = nx.complete_graph(N).to_directed()
-M = G.number_of_edges()
 weights = np.random.randint(1, 10, size=(N, N))
 weights_dict = {(u, v): {'weight': weights[u, v]} for u, v in G.edges}
 nx.set_edge_attributes(G, weights_dict)
@@ -32,12 +34,12 @@ print('end positions:', end_positions)
 print('creating model...')
 
 model = LpProblem('tsp', LpMinimize)
-variable_names = ['{{{}:{},{}}}'.format(a, u, v) for a in range(A) for u in range(N) for v in range(N)]
+variable_names = ['{{{}:{},{}}}'.format(a, u, v) for a in agents for u in nodes for v in nodes]
 variables = np.array(LpVariable.matrix('X', variable_names, cat=LpBinary)).reshape((A, N, N))
 
 print('fixing some unused variables to zero...')
-# self referring arc
-for v in variables[:, np.arange(N), np.arange(N)].reshape((-1,)):
+# self referring arc (entries on diagonal)
+for v in variables[:, nodes, nodes].reshape((-1,)):
     v.setInitialValue(0)
     v.fixValue()
 
@@ -52,47 +54,45 @@ for v in variables[:, end_positions, :].reshape((-1,)):
     v.fixValue()
     
 # arcs out of start nodes of wrong agents
-for a, p in enumerate(start_positions):
-    for v in variables[np.arange(A) != a, p, :].reshape((-1,)):
+for a in agents:
+    for v in variables[agents != a, start_positions[a], :].reshape((-1,)):
         v.setInitialValue(0)
         v.fixValue()
     
 # arcs into end nodes of wrong agents
-for a, p in enumerate(end_positions):
-    for v in variables[np.arange(A) != a, :, p].reshape((-1,)):
+for a in agents:
+    for v in variables[agents != a, :, end_positions[a]].reshape((-1,)):
         v.setInitialValue(0)
         v.fixValue()
 
 obj_func = lpSum(variables * weights)
 model += obj_func
 
-print('creating degree inequalities of start positions...')
-for a, p in enumerate(start_positions):
-    outStartIneq = lpSum(variables[a, p, :]) == 1
+print('creating degree inequalities of start and end positions...')
+for a in agents:
+    outStartIneq = lpSum(variables[a, start_positions[a], :]) == 1
+    inEndIneq = lpSum(variables[a, :, end_positions[a]]) == 1
     model += outStartIneq, 'outdegree inequality of start position of {}'.format(a)
-
-print('creating degree inequalities of end positions...')
-for a, p in enumerate(end_positions):
-    inEndIneq = lpSum(variables[a, :, p]) == 1
     model += inEndIneq, 'indegree inequality of end position of {}'.format(a)
 
 print('creating degree inequalities for ordinary nodes...')
-for n in range(N):
+for n in nodes:
     if not n in start_positions and not n in end_positions:
         inDegreeIneq = lpSum(variables[:, :, n]) == 1
         outDegreeIneq = lpSum(variables[:, n, :]) == 1
         model += inDegreeIneq, 'indegree inequality ' + str(n)
         model += outDegreeIneq, 'outdegree inequality ' + str(n)
-        for a in range(A):
+        for a in agents:
             perAgentDegreesIneq = lpSum(variables[a, :, n]) == lpSum(variables[a, n, :])
             model += perAgentDegreesIneq, 'per agent degree inequality {} {}'.format(a, n)
     
     
 print('creating subtour elimination inequalities...')
 for s in tqdm(range(2, N)):
-    for S in combinations(range(1, N), s):
-        S = list(S)
-        for a in range(A):
+    for a in agents:
+        sa, ea = start_positions[a], end_positions[a]
+        for S in combinations(nodes[np.logical_and(nodes != sa, nodes != ea)], s):
+            S = list(S)
             subTourEl = lpSum(variables[(a,) + np.ix_(S, S)]) <= s - 1
             model += subTourEl, 'subtour elimination {} {}'.format(a, S)
 
